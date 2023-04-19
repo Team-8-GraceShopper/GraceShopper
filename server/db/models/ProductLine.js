04701e5973c9299ce3ac91cd91ec6a1b8bcef40d
phx_admin <rasparks303@gmail.com>
2023-04-19 09:41:52 -0600

    WiP db seed

---
+++ server/db/models/ProductLine.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const ProductLine = db.define("productLine", {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.TEXT,
+    },
+});
+
+module.exports = ProductLine;
