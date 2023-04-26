2420acf5010c3fa3ff87b43c4402647ee36fbb1f
vdaquaro13 <vdaquaro13@gmail.com>
2023-04-26 02:16:02 -0400

    added some css

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -2,7 +2,12 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 const CartProduct = require("./CartProduct");
 
-const Cart = db.define("cart", {});
+const Cart = db.define("cart", {
+  isProcessed: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  }});
 
 Cart.hasMany(CartProduct);
 
