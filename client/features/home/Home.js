fbe5e03a76560a07539533bf26364ed75e18c687
Hussein Aligabi <aligabih@gmail.com>
2023-04-26 13:28:29 -0400

    more and more css changes

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -11,19 +11,25 @@ const Home = () => {
           <div class="crawl">
             <div class="title">
               <p>Episode 2301</p>
-              <h1>Team 8 Graceshopper</h1>
+              <h1>Team 8</h1>
             </div>
 
             <p>
-            It is a dark time for the Cohort. Although Junior Phase has been destroyed, Fullstack mentors have driven the Cohort forces from their hidden base and pursued them across the galaxy.
+              It is a dark time for the Cohort. Although Junior Phase has been
+              destroyed, Fullstack mentors have driven the Cohort forces from
+              their hidden base and pursued them across the galaxy.
             </p>
 
             <p>
-            Evading the dreaded Fullstack Starfleet, a group of Cohort fighters led by Devonte, Hussein and Vincent have established a new secret base on the remote ice world of Github.
+              Evading the dreaded Fullstack Starfleet, a group of Cohort
+              fighters led by Devonte, Hussein and Vincent have established a
+              new secret base on the remote ice world of Github.
             </p>
 
             <p>
-            The evil lord Darth Orlando, obsessed with finding one word functions, has dispatched thousands of remote probes into the far reaches of space....
+              The evil lord Darth Orlando, obsessed with finding one word
+              functions, has dispatched thousands of remote probes into the far
+              reaches of space....
             </p>
           </div>
         </section>
